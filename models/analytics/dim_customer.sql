04cc0fc908f3503844c83dedbc79419a26c90242
NhuLamNg <lamnhu074@gmail.com>
2022-11-19 16:19:00 +0700

    description_dim.customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,22 @@
-SELECT 
+WITH
+  customer AS (
+  SELECT
+    *
+  FROM
+    `vit-lam-data.wide_world_importers.sales__customers` ),
+  customer_change_name AS (
+  SELECT
+    customer_id AS customer_key,
+    customer_name AS customer_name
+  FROM
+    customer ),
+  customer_type AS(
+  SELECT
+    CAST(customer_key AS integer) AS customer_key,
+    CAST(customer_name AS string) AS customer_name
+  FROM
+    customer_change_name )
+SELECT
   *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+FROM
+  customer_type

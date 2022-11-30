4f77055795b5c2b2c0450834f4e8ea061372fc01
NhuLamNg <lamnhu074@gmail.com>
2022-11-30 14:37:57 +0700

    sua lai format query

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -2,18 +2,25 @@ with dim_product__source as (
   select *
   from `vit-lam-data.wide_world_importers.warehouse__stock_items`
 )
-,dim_product_name_column as(
-SELECT 
-  stock_item_id  as product_key
-  ,stock_item_name  as product_name
-  ,brand as brand_name
-FROM dim_product__source )
-,dim_product_cast_type as (
+
+,dim_product_rename__column as(
+  SELECT 
+    stock_item_id  as product_key
+    ,stock_item_name  as product_name
+    ,brand as brand_name
+  FROM dim_product__source ) 
+
+,dim_product__cast_type as (
   select
-  cast(product_key as integer) as product_key
-  ,cast(product_name as string) as product_name
-  ,cast(brand_name as string) as brand_name
-  from dim_product_name_column
+    cast(product_key as integer) as product_key
+    ,cast(product_name as string) as product_name
+    ,cast(brand_name as string) as brand_name
+  from dim_product_rename__column
 )
-select * from dim_product_cast_type
+
+select 
+  product_key
+  ,product_name
+  ,brand_name
+from dim_product__cast_type
 

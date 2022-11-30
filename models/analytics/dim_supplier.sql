1cb11e5726a1270ad4692e83112d9e7704df1403
NhuLamNg <lamnhu074@gmail.com>
2022-11-30 16:35:01 +0700

    create supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,23 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+
+with dim_supplier__source  as (
+  select *
+  from `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+,dim_supplier_rename__column as(
+  SELECT 
+    supplier_id as supplier_key
+    ,supplier_name as supplier_name
+  FROM dim_supplier__source ) 
+
+,dim_supplier__cast_type as (
+  select
+      cast(supplier_key as integer) as supplier_key
+      ,cast(supplier_name as string) as supplier_name
+  from dim_supplier_rename__column
+)
+
+select 
+  supplier_key
+  ,supplier_name 
+from dim_supplier__cast_type

faec2a06c2683ef2eb8630e0dd2c08aa7c084352
NhuLamNg <lamnhu074@gmail.com>
2022-11-30 15:42:40 +0700

    change query

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,38 +1,53 @@
-with fact_sale_order as (
-  select
-    *
-  from
-    `vit-lam-data.wide_world_importers.sales__order_lines`
-),
-fact_sale_name_column as (
+WITH fact_sales_order_line__source AS (
+  SELECT * 
+  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, fact_sales_order_line__rename_column AS (
+  SELECT 
+    order_id AS sales_order_key
+    , order_line_id AS sales_order_line_key
+    , stock_item_id AS product_key
+    , quantity 
+    , unit_price
+  FROM 
+    fact_sales_order_line__source
+)
+
+, fact_sales_order_line__cast_type AS (
+  SELECT 
+    CAST(sales_order_key AS INTEGER) AS sales_order_key
+    , CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key
+    , CAST(product_key AS INTEGER) AS product_key
+    , CAST(quantity AS INTEGER) AS quantity
+    , CAST(unit_price AS NUMERIC) AS unit_price
+  FROM 
+    fact_sales_order_line__rename_column
+)
+
+, fact_sales_order_line__calculate_measure AS (
   SELECT
-    order_line_id AS sales_order_line_key,
-    quantity as quantity,
-    unit_price as unit_price,
-    quantity * unit_price as gross_amount,
-    stock_item_id as product_key
-  FROM
-    fact_sale_order
-),
-fact_sale_cast_type as (
-  select
-    cast(sales_order_line_key as integer) as sales_order_line_key,
-    cast(quantity as integer) as quantity,
-    cast(unit_price as numeric) as unit_price,
-    cast(quantity as integer) * cast(unit_price as numeric) as gross_amount,
-    cast (product_key as integer) as product_key
-  from
-    fact_sale_name_column
+    *
+    , quantity * unit_price AS gross_amount
+  FROM 
+    fact_sales_order_line__cast_type
 )
-,final as(
-select
-  *
-from
-  fact_sale_cast_type
+
+, fact_sales_order_line__join_stg_fact_sales_order AS (
+  SELECT
+    fact_line.*
+    , fact_header.customer_key
+  FROM fact_sales_order_line__calculate_measure fact_line
+  LEFT JOIN {{ ref('stg_fact_sales_order') }} fact_header
+  ON fact_line.sales_order_key = fact_header.sales_order_key
 )
-select 
-  a.*
-  ,b.*
-from final a
-left join {{ ref('stg_fact_sales_order')}} b
-on a.sales_order_line_key = b.sale_order_key
+
+SELECT 
+  sales_order_line_key
+  , sales_order_key
+  , customer_key
+  , product_key
+  , quantity
+  , unit_price
+  , gross_amount
+FROM fact_sales_order_line__join_stg_fact_sales_order
